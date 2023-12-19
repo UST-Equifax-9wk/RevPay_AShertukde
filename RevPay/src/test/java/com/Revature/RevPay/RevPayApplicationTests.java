a89d75ad1cafbbb401a04d9024ba651992b9ae63
ashertukde <ashertukde@csub.edu>
2023-12-18 18:49:00 -0800

    setting up

---
+++ RevPay/src/test/java/com/Revature/RevPay/RevPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Revature.RevPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
